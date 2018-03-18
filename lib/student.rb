28d557be83aeb0f4351c0ba3f75be5f5c733fdc3
Rani Shah <rani76@gmail.com>
2018-03-18 05:14:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student  < User
+
+def initialize
+  @first_name = first_name
+  @last_name = last_name
+  @knowledge = []
+end
+
+def learn(string)
+  @knowledge << string
+end
+
+def knowledge
+  @knowledge
+end 
 
 end
